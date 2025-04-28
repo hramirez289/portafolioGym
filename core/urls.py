5f57cb7ac63a6f9933ef1ec1e86395574fc826a6
gatino17 <gatino.osses@hotmail.com>
2025-04-28 12:21:50 -0400

    Planes agregados con detalle y precios

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import index
-
-urlpatterns = [
-    path('', index, name="index")
-=======
-from .views import index, registro, contacto
+from .views import index, registro, contacto, seleccionar_plan
 
 urlpatterns = [
     path('', index, name="index"),
     path('registro', registro, name="registro"),
-    path('contacto', contacto, name="contacto")
->>>>>>> 7bd92602f268b218a9574e8ccdae7d5ed7416467
+    path('contacto', contacto, name="contacto"),
+    path('planes/<str:plan_nombre>/<int:precio>/', seleccionar_plan, name='seleccionar_plan'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-def index (request):
-<<<<<<< HEAD
-    return render(request, 'index.html')
-=======
+def index(request):
     return render(request, 'index.html')
 
-def registro (request):
+def registro(request):
     return render(request, 'registro.html')
 
-def contacto (request):
+def contacto(request):
     return render(request, 'contacto.html')
->>>>>>> 7bd92602f268b218a9574e8ccdae7d5ed7416467
+
+def seleccionar_plan(request, plan_nombre, precio):
+    return render(request, 'planes.html', {'plan': plan_nombre, 'precio': precio})
+
