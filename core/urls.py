207d5d65d49f92038e728efca6c7af734aae3c23
gatino17 <gatino.osses@hotmail.com>
2025-04-28 15:45:59 -0400

    Acceso admin por RUT, panel admin y detalles de plan por usuario

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import index, registro, contacto, seleccionar_plan
+from .views import index, registro, contacto, seleccionar_plan,  mi_espacio, panel_admin
 
 urlpatterns = [
     path('', index, name="index"),
     path('registro', registro, name="registro"),
     path('contacto', contacto, name="contacto"),
     path('planes/<str:plan_nombre>/<int:precio>/', seleccionar_plan, name='seleccionar_plan'),
+    path('mi-espacio/', mi_espacio, name='mi_espacio'),
+    path('admin-panel/', panel_admin, name='panel_admin'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import ContratoPlan
 from django.contrib import messages
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta  # Para sumar meses
+from django.db.models import Sum, Count
+
+ADMIN_RUT = '99999999-9'
 
 def index(request):
     return render(request, 'index.html')
@@ -37,3 +42,36 @@ def seleccionar_plan(request, plan_nombre, precio):
         return redirect('index')  # Redirige al inicio
 
     return render(request, 'planes.html', {'plan': plan_nombre, 'precio': precio})
+
+
+def mi_espacio(request):
+    if request.method == 'POST':
+        rut = request.POST.get('rut')
+        
+        # 🚨 Validación para RUT de Administrador
+        if rut == ADMIN_RUT:
+            return redirect('panel_admin')
+
+        try:
+            contrato = ContratoPlan.objects.filter(rut=rut).latest('fecha_contratacion')
+            fecha_inicio = contrato.fecha_contratacion
+            fecha_termino = fecha_inicio + relativedelta(months=1)
+            return render(request, 'mi_espacio.html', {
+                'contrato': contrato,
+                'fecha_inicio': fecha_inicio,
+                'fecha_termino': fecha_termino
+            })
+        except ContratoPlan.DoesNotExist:
+            return render(request, 'index.html', {'error': 'RUT no encontrado. Verifica e intenta nuevamente.'})
+
+    return redirect('index')
+
+
+def panel_admin(request):
+    total_contratos = ContratoPlan.objects.count()
+    total_monto = ContratoPlan.objects.aggregate(Sum('precio'))['precio__sum'] or 0
+
+    return render(request, 'panel_admin.html', {
+        'total_contratos': total_contratos,
+        'total_monto': total_monto
+    })
